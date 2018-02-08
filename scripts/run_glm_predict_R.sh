77b419e9a7a73b264f413d3e9fc165824ff6df62
bhkoo <bhkoo5@gmail.com>
2018-02-08 10:24:44 -0500

    Fixed assessment names

--- scripts/run_glm_predict_R.sh
+++ scripts/run_glm_predict_R.sh
@@ -3,7 +3,7 @@
 # run glm_predict.R in parallel
 # Bonhwang Koo
 # 2/6/2018
-# Command line: cat run_glm_predict_R.sh | parallel -j 10
+# Command line: cat run_glm_predict_R.sh | parallel -j 7
 
 Rscript --vanilla glm_predict.R "WASI_FSIQ"
 Rscript --vanilla glm_predict.R "WASI_PRI_Comp"
